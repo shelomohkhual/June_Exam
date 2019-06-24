2115d906c93f14cf725ae640c7ac6cefd891839f
Shelo <YOUR@EMAIL.com>
2019-06-24 13:01:26 +0800

    going lunch

--- UNO.rb
+++ UNO.rb
@@ -17,22 +17,103 @@
 # - if someone finished, show finisher and show all players left over cards and sum to finshers score
 # - and show back the scoreboard and reset everything excep the scores.
 
+############################### OOP - CODE ############################### 
+# DONE ----------------------------
+class Player
+    attr_accessor :players, :qty_of_player, :qty_of_human_player,:qty_of_bot_player
+    def initialize(qty_of_human_player,qty_of_player))
+        @players=[]
+        @qty_of_player=qty_of_player
+        @qty_of_human_player=qty_of_human_player
+        @qty_of_bot_player=0
+
+    end
+
+    def create_players
+        @qty_of_bot_player=@qty_of_player-@qty_of_human_player
+        self.create_human_player
+        self.create_bot_player
+    end
+
+    def create_human_player
+        count=0
+        @qty_of_human_player.times do
+            name="Human_Player(#{count+1})"
+            name=Human_Player.new
+            @qty_of_player.push name
+            count += 1
+        end
+    end
     
-class Score_Board
+    def create_bot_player
+        count=0
+        @qty_of_bot_player.times do
+            bot="Bot_Player(#{count+1})"
+            bot=Bot_Player.new
+            @qty_of_player.push bot
+            count += 1
+        end
+    end
+
+end
+
+class Human_Player
+    @holding_cards=
+
+end
+
+class Bot_Player
+
+end
 
 class Game
+    attr_accessor :players
+    def initialize(array_players)
+        @players=array_players
+    end
+
+    def showboard
+    end
+
+    def final_score
+        puts winner is
+        show Score_Board
+    end
 end
 
-class Player
-end
-class Human_Player < Player
-
-end
-class Bot_player < Player
-
+class Cards
 end
 
+# DONE ----------------------------
+class Score_Board
+    attr_accessor :player_scores, :winner
+    def initialize
+        @player_scores={|player,scores|player[score]=[]}
+        @winner=nil
+    end
+
+    def show_table_score
+        highest_point=@player_scores.sort_by{|k, v| -v}.to_h
+        highest_point.each do |player,scores|
+            puts "Player : #{player} | Point : #{point}"
+        end
+    end
+
+    def winner?
+        @player_scores.each do |player,scores|
+            if scores.sum > 100
+                @winner=player
+                return true
+            else nil
+            end
+        end
+        return false 
+    end
+end
+
+    
 ############################### DRIVER - CODE ############################### 
+
 puts "how many player?"
 qty_of_player=gets.chomp.to_i
 
@@ -42,8 +123,9 @@ qty_of_human_player=gets.chomp.to_i
 qty_of_bot_player=qty_of_player-qty_of_human_player
 
 Player.new(qty_of_human_player,qty_of_player)
-Player.creat_human_player
-Player.creat_bot_player
+Player.create_players
+# Player.creat_human_player
+# Player.creat_bot_player
 start_game=Game.new(Player.players)
 start_game.final_score
 
